f697786eb6665a9ea35eb605cf6a4caec0dbf3a4
Juba <rgjuba@gmail.com>
2019-01-01 15:04:25 -0200

    component navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './ui/NavBar';
 
 class App extends Component {
   render() {
+    const logo = 'Test Dev';
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            React With Bootstrap 4 - Agile App
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            In Development
-          </a>
-        </header>
+      <div className="container">
+        <NavBar logo={logo}/>
       </div>
     );
   }
